78883a794fadc66c003d43d8da7722cc38d39b05
xmvziron <103202084+xmvziron@users.noreply.github.com>
2026-08-16 18:01:58 +0200

    Rematch TitleScreen functions
    Somehow these are no longer 100% accuracy. So fix that.
    TitleScreen::OnUpdateSpellStageSelect, TitleScreen::OnUpdateSpellCardSelect,
    TitleScreen::OnUpdateReplayMenu and
    TitleScreen::DrawCompletionStatusText are still not matching.

--- src/TitleScreen.cpp
+++ src/TitleScreen.cpp
@@ -2291,7 +2291,7 @@ ChainCallbackResult TitleScreen::OnUpdateSpellCardSelect()
                 {
                     this->cursor = this->currentNumberOfSpellCards - 1;
                 }
-                if (this->currentNumberOfSpellCards >= this->cursor)
+                if (this->cursor >= this->currentNumberOfSpellCards)
                 {
                     this->cursor = 0;
                 }
@@ -2312,7 +2312,7 @@ ChainCallbackResult TitleScreen::OnUpdateSpellCardSelect()
                     {
                         this->cursor = currentNumberOfSpellCards - 1;
                     }
-                    if (this->currentNumberOfSpellCards >= this->cursor)
+                    if (this->cursor >= this->currentNumberOfSpellCards)
                     {
                         this->cursor = currentNumberOfSpellCards - 1;
                     }
@@ -2522,11 +2522,11 @@ ChainCallbackResult TitleScreen::DrawReplayMenu()
         g_AsciiManager.SetIsSelected(i == this->selectedReplay);
         if (i == this->selectedReplay)
         {
-            g_AsciiManager.SetColor(COLOR_WHITE);
+            g_AsciiManager.color.d3dColor = COLOR_WHITE;
         }
         else
         {
-            g_AsciiManager.SetColor(0xff808080);
+            g_AsciiManager.color.d3dColor = 0xff808080;
         }
 
         if (this->replays[i].spellcardNumber < 0)
@@ -2547,7 +2547,7 @@ ChainCallbackResult TitleScreen::DrawReplayMenu()
 
     if ((this->currentScreenState == 2 || this->currentScreenState == 3) && this->currentReplay)
     {
-        g_AsciiManager.SetColor(COLOR_WHITE);
+        g_AsciiManager.color.d3dColor = COLOR_WHITE;
         g_AsciiManager.SetIsSelected(FALSE);
 
         vm = &this->vms[78];
@@ -2574,22 +2574,22 @@ ChainCallbackResult TitleScreen::DrawReplayMenu()
 
                     if (i2 == this->selectedReplayStage)
                     {
-                        g_AsciiManager.SetColor(0xffffffff);
+                        g_AsciiManager.color.d3dColor = 0xffffffff;
                     }
                     else
                     {
-                        g_AsciiManager.SetColor(0xff808080);
+                        g_AsciiManager.color.d3dColor = 0xff808080;
                     }
                 }
                 else
                 {
                     if (i2 == this->selectedReplayStage)
                     {
-                        g_AsciiManager.SetColor(0x60ffffff);
+                        g_AsciiManager.color.d3dColor = 0x60ffffff;
                     }
                     else
                     {
-                        g_AsciiManager.SetColor(0x60808080);
+                        g_AsciiManager.color.d3dColor = 0x60808080;
                     }
                 }
 
@@ -2623,7 +2623,7 @@ ChainCallbackResult TitleScreen::DrawReplayMenu()
         }
     }
 
-    g_AsciiManager.SetColor(COLOR_WHITE);
+    g_AsciiManager.color.d3dColor = COLOR_WHITE;
     g_AsciiManager.SetIsSelected(FALSE);
 
     return CHAIN_CALLBACK_RESULT_CONTINUE;
@@ -2637,7 +2637,7 @@ ChainCallbackResult TitleScreen::DrawPracticeStageSelect()
     i32 i;
     AnmVm *vm;
 
-    g_AsciiManager.SetColor(COLOR_WHITE);
+    g_AsciiManager.color.d3dColor = COLOR_WHITE;
     g_AsciiManager.SetIsSelected(FALSE);
 
     vm = &this->vms[141];
@@ -2666,15 +2666,15 @@ ChainCallbackResult TitleScreen::DrawPracticeStageSelect()
 
         if (i == this->cursor)
         {
-            g_AsciiManager.SetColor(COLOR_WHITE);
+            g_AsciiManager.color.d3dColor = COLOR_WHITE;
         }
         else if (IS_STAGE_CLEARED(clearInfo, i))
         {
-            g_AsciiManager.SetColor(0xffa0a0a0);
+            g_AsciiManager.color.d3dColor = 0xffa0a0a0;
         }
         else
         {
-            g_AsciiManager.SetColor(0xff404040);
+            g_AsciiManager.color.d3dColor = 0xff404040;
         }
 
         g_AsciiManager.AddFormatText(
@@ -2684,7 +2684,7 @@ ChainCallbackResult TitleScreen::DrawPracticeStageSelect()
         position.y += 16.0f;
     }
 
-    g_AsciiManager.SetColor(COLOR_WHITE);
+    g_AsciiManager.color.d3dColor = COLOR_WHITE;
     g_AsciiManager.SetIsSelected(FALSE);
 
     return CHAIN_CALLBACK_RESULT_CONTINUE;
@@ -2726,7 +2726,7 @@ ChainCallbackResult TitleScreen::DrawSpellStageSelect()
     totalAttemptedLastWordPerShot = 0;
     totalAttemptedLastWord = 0;
 
-    g_AsciiManager.SetColor(COLOR_WHITE);
+    g_AsciiManager.color.d3dColor = COLOR_WHITE;
     g_AsciiManager.SetIsSelected(FALSE);
 
     vm = &this->vms[141];
@@ -2744,11 +2744,11 @@ ChainCallbackResult TitleScreen::DrawSpellStageSelect()
         g_AsciiManager.SetIsSelected(i == this->cursor);
         if (i == this->cursor)
         {
-            g_AsciiManager.SetColor(COLOR_WHITE);
+            g_AsciiManager.color.d3dColor = COLOR_WHITE;
         }
         else
         {
-            g_AsciiManager.SetColor(0xffa0a0a0);
+            g_AsciiManager.color.d3dColor = 0xffa0a0a0;
         }
 
         capturesPerStageSpellPracticePerShot = 0;
@@ -2823,7 +2823,7 @@ ChainCallbackResult TitleScreen::DrawSpellStageSelect()
     position.y += 5.0f;
 
     g_AsciiManager.SetIsSelected(FALSE);
-    g_AsciiManager.SetColor(0xffd06060);
+    g_AsciiManager.color.d3dColor = 0xffd06060;
 
     g_AsciiManager.AddFormatText(&position, "%sTotal",
                                  (totalCapturesSpellPracticePerShot >= SPELLCARD_COUNT_SPELLCARDS
@@ -2840,7 +2840,7 @@ ChainCallbackResult TitleScreen::DrawSpellStageSelect()
     position.x -= 182.0f;
 
     g_AsciiManager.SetScale(1.0f, 1.0f);
-    g_AsciiManager.SetColor(COLOR_WHITE);
+    g_AsciiManager.color.d3dColor = COLOR_WHITE;
     g_AsciiManager.SetIsSelected(FALSE);
 
     this->spellCardNameVms[1].pos.x = 400.0f;
@@ -2988,7 +2988,7 @@ ChainCallbackResult TitleScreen::DrawSpellCardSelect()
     u16 clearInfo;
     Float3 position;
 
-    g_AsciiManager.SetColor(COLOR_WHITE);
+    g_AsciiManager.color.d3dColor = COLOR_WHITE;
     g_AsciiManager.SetIsSelected(FALSE);
 
     position = Float3(16.0f, 78.0f, 0.0f);
@@ -3057,7 +3057,7 @@ ChainCallbackResult TitleScreen::DrawSpellCardSelect()
     g_AnmManager->DrawNoRotation(&this->spellCardInfoVms[5]);
     g_AnmManager->DrawNoRotation(&this->spellCardInfoVms[6]);
 
-    g_AsciiManager.SetColor(COLOR_WHITE);
+    g_AsciiManager.color.d3dColor = COLOR_WHITE;
     g_AsciiManager.SetIsSelected(FALSE);
 
     return CHAIN_CALLBACK_RESULT_CONTINUE;
